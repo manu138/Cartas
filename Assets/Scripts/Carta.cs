dcf604180a5e70aa28b3d30f3a5f2ce1839649d9
Manuela Recio <manu138@hotmail.es>
2017-02-24 18:57:10 -0500

    commit

---
+++ Assets/Scripts/Carta.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carta : MonoBehaviour
+{
+    public CardSuit Suit
+    {
+        get;
+        private set;
+    }
+
+    public CardNumber number
+    {
+        get;
+        set;
+    }
+
+   public Carta(CardSuit suit, CardNumber value)
+    {
+       
+        Suit = suit;
+        number = value;
+    }
+    public enum CardNumber
+    {
+        A = 1, dos = 2, tres = 3, cuatro = 4, cinco = 5, seis = 6, siete = 7, ocho = 8, nueve = 9, J = 10, Q = 11, K = 12
+
+    }
+
+
+    public enum CardSuit
+    {
+        Spades = 1,
+        Hearts = 2,
+        Diamonds = 3,
+        Clubs = 4
+    }
+
+}
+
+
+
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public  class Deck
+
+{
+
+    private Stack<Carta> cards = new Stack<Carta>();
+
+    public static void GetaCard(Carta[] cards)
+    {
+        for (int i = 0; i < cards.Length; i++)
+        {
+            Debug.Log(cards[i]);
+        }
+    }
+
+   // public static void Shuffle(this Stack<Carta> cards)
+      //   {
+
+   // }
+
+   
+    public Deck()
+    {
+       
+        Carta.CardSuit[] palos= (Carta.CardSuit[])System.Enum.GetValues(typeof(Carta.CardSuit));
+        Carta.CardNumber[] values = (Carta.CardNumber[])System.Enum.GetValues(typeof(Carta.CardNumber));//los trae como T
+        foreach (Carta.CardNumber valorCarta in values)
+        {
+            foreach (Carta.CardSuit palo in palos)
+            { 
+                cards.Push(new Carta(palo, valorCarta));
+            }
+        }
+    
+
+      
+
+
+    }
+
+
+
+
+
+
+}
